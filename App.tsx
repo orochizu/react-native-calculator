2198790f0d039eaba7f9736ac03f089442272b68
Michal Bunkowskiu <michalbunkowski@gmail.com>
2020-04-03 16:43:48 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.message}>Tutaj stworzymy naszą pierwszą aplikację mobilną przy pomocy React Native!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center'
+  }
+});
